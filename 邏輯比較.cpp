a0ac6802216864e7c5fb72b204ef059eaccafe19
s1130253-Nemo <s1130253@dmhs.kh.edu.tw>
2026-03-10 11:46:08 +0800

    Create 邏輯比較.cpp

---
+++ 邏輯比較.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	cout<<(5>=2)<<endl;
+	cout<<(5<=2)<<endl;
+	cout<<(5>2)<<endl;
+	cout<<(5<2)<<endl;
+	cout<<(5==2)<<endl;
+	cout<<(5!=2)<<endl;
+	return 0;
+}
