fb0ab642cbb4cb43b42ddd9d6e6723c700b8e330
s1130253-Nemo <s1130253@dmhs.kh.edu.tw>
2026-04-28 11:35:57 +0800

    Create function99.cpp

---
+++ function99.cpp
@@ -0,0 +1,27 @@
+using namespace std;
+
+int main() {
+	for (int i = 1; i <= 9; i++) {
+		for (int j = 1; j <= 3; j++) {
+			cout << j << "*" << i << "=" << j*i << " ";
+		}
+		cout << "\n";
+	}
+	cout << "-----------------\n";
+	for (int i = 1; i <= 9; i++) {
+		for (int j = 4; j <= 6; j++) {
+			cout << j << "*" << i << "=" << j*i << " ";
+		}
+		cout << "\n";
+	}
+	cout << "-----------------\n";
+	for (int i = 1; i <= 9; i++) {
+		for (int j = 7; j <=#include <iostream>
+ 9; j++) {
+			cout << j << "*" << i << "=" << j*i << " ";
+		}
+		cout << "\n";
+	}
+	cout << "-----------------\n";
+	return 0;
+}
